ddd3cece42fc780b3088e3e238c43c0886d072b7
Juliano Souza <souzza.s1990@gmail.com>
2020-08-04 16:38:16 -0300

    finished

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -1,22 +1,38 @@
-import { inject, injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe'
 
-import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError'
 
-import Customer from '../infra/typeorm/entities/Customer';
-import ICustomersRepository from '../repositories/ICustomersRepository';
+import Customer from '../infra/typeorm/entities/Customer'
+import ICustomersRepository from '../repositories/ICustomersRepository'
 
 interface IRequest {
-  name: string;
-  email: string;
+  name: string
+  email: string
 }
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkCustomerExists = await this.customersRepository.findByEmail(
+      email,
+    )
+
+    if (checkCustomerExists) {
+      throw new AppError('Email address already used.')
+    }
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    })
+
+    return customer
   }
 }
 
-export default CreateCustomerService;
+export default CreateCustomerService
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,19 +1,19 @@
-import { getRepository, Repository, In } from 'typeorm';
+import { getRepository, Repository, In } from 'typeorm'
 
-import IProductsRepository from '@modules/products/repositories/IProductsRepository';
-import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
-import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
-import Product from '../entities/Product';
+import IProductsRepository from '@modules/products/repositories/IProductsRepository'
+import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO'
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO'
+import Product from '../entities/Product'
 
 interface IFindProducts {
-  id: string;
+  id: string
 }
 
 class ProductsRepository implements IProductsRepository {
-  private ormRepository: Repository<Product>;
+  private ormRepository: Repository<Product>
 
   constructor() {
-    this.ormRepository = getRepository(Product);
+    this.ormRepository = getRepository(Product)
   }
 
   public async create({
@@ -21,22 +21,47 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    })
+
+    await this.ormRepository.save(product)
+
+    return product
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const findProduct = await this.ormRepository.findOne({ where: { name } })
+
+    return findProduct
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const findProducts = await this.ormRepository.findByIds(products)
+
+    return findProducts
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const FindProducts = await this.findAllById(
+      products.map(product => ({ id: product.id })),
+    )
+
+    const updatedProducts = FindProducts.map(product => ({
+      ...product,
+      quantity:
+        product.quantity -
+        (products.find(({ id }) => id === product.id)?.quantity || 0),
+    }))
+
+    await this.ormRepository.save(updatedProducts)
+
+    return updatedProducts
   }
 }
 
-export default ProductsRepository;
+export default ProductsRepository
